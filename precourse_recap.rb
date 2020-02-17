a754d566fe63b85e9aae1850740a882991b683d6
mqprogramming <matt.qgley@gmail.com>
2020-02-17 16:25:34 +0000

    first commit

---
+++ precourse_recap.rb
@@ -0,0 +1,26 @@
+# Conditional
+if (name = "Matthew")
+  puts "My name!"
+else
+  puts "Not my name..."
+end
+
+# Function
+def new_Function
+  p "This is a new function"
+end
+
+# Array
+new_array = ["item1", "item2", "item3"]
+
+# Hash
+new_hash = {"key1" => "value1", "key2" => "value2"}
+
+# Loop
+i = 5
+a_number = 0
+
+while (i < 0)
+  a_number = a_number + 1
+  i - 1
+end
